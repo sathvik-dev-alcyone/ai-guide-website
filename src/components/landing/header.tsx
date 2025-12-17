4ba46775b93d101388502ed5c0d4a9ac3034b89f
sathvik 707 <sathviks707satu@gmail.com>
2025-12-17 23:01:17 +0000

    see the headding is same colour as background it dont look diffrece so m

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -2,7 +2,7 @@ import { Wrench } from 'lucide-react';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-white/10 bg-background/30 backdrop-blur-lg">
+    <header className="sticky top-0 z-50 w-full border-b border-white/10 bg-background/50 backdrop-blur-lg">
       <div className="container mx-auto flex h-16 items-center">
         <div className="mr-auto flex items-center">
           <Wrench className="h-6 w-6 mr-2 text-primary" />
